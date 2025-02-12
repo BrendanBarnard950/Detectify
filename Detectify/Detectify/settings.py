2626019fba9da77de6ebc69968323af6a743d1a0
Brendan Barnard <brendan007barnard@gmail.com>
2025-02-13 00:25:10 +0200

    Updated settings to account for nuking

--- Detectify/Detectify/settings.py
+++ Detectify/Detectify/settings.py
@@ -38,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'videofeed',
-    'notification',
-    'frontend',
 ]
 
 MIDDLEWARE = [
