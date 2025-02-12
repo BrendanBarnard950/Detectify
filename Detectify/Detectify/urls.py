7e59edb0e51cf5440c56af17ed487e7e5b8f6420
Brendan Barnard <brendan007barnard@gmail.com>
2025-02-13 00:24:16 +0200

    Fixed broken urls after deleting large sections

--- Detectify/Detectify/urls.py
+++ Detectify/Detectify/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('videofeed/', include('videofeed.urls')),
-    path('notifications/', include('notification.urls')),
-    path('frontend/', include('frontend.urls')),
+    path('', include('videofeed.urls')),
 ]
--- Detectify/videofeed/urls.py
+++ Detectify/videofeed/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('live_feed/', views.live_feed_page, name='live_feed_page'),
-    path('live_feed/stream/', views.live_feed_stream, name='live_feed_stream'),
-    path('live_feed/label/', views.live_feed_label, name='live_feed_label'),
-    path('process_image/', views.process_image, name='process_image'),
+    path('', views.live_feed_page, name='live_feed_page'),
+    path('stream/', views.live_feed_stream, name='live_feed_stream'),
+    path('label/', views.live_feed_label, name='live_feed_label'),
 ]
